b09e5919fdade69143c343702c31c77eca76c00a
flash <developsoon@gmail.com>
2023-02-28 17:43:14 +0900

    feat: add font colors pallete & logo image in settings screen UI

--- lib/view/settings_screen.dart
+++ lib/view/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 import '../models/settings.dart';
@@ -11,118 +12,160 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final SettingsModel model = context.watch<SettingsModel>();
     final t = AppLocalizations.of(context)!;
+    var theme = Theme.of(context);
+
+    final List<Color> colorPallet = [
+      theme.colorScheme.onSurface,
+      theme.colorScheme.copyWith(primary: const Color(0xff4455ba)).primary,
+      theme.colorScheme.copyWith(secondary: const Color(0xff5b5d72)).secondary,
+      theme.colorScheme.tertiary,
+      theme.colorScheme.copyWith(primary: const Color(0xff7889f1)).primary,
+      theme.colorScheme.copyWith(secondary: const Color(0xff8d8fa6)).secondary
+    ];
 
     return Scaffold(
-      appBar: AppBar(),
       body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            SizedBox(
-              height: 72,
-              child: ListTile(title: Text(t.settingsThemeModeTitle)),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
-              child: SegmentedButton<ThemeMode>(
-                segments: <ButtonSegment<ThemeMode>>[
-                  for (var themeMode in ThemeMode.values)
-                    ButtonSegment(
-                      value: themeMode,
-                      label: Text(t.settingsThemeModeValue(themeMode.name)),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 86),
+              model.themeMode == ThemeMode.dark
+                  ? SvgPicture.asset('assets/logo_dark.svg')
+                  : SvgPicture.asset('assets/logo.svg'),
+              Expanded(
+                child: ListView(
+                  physics: const ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 72,
+                      child: ListTile(title: Text(t.settingsThemeModeTitle)),
                     ),
-                ],
-                showSelectedIcon: true,
-                selected: {model.themeMode},
-                onSelectionChanged: (selected) {
-                  model.themeMode = selected.first;
-                },
-              ),
-            ),
-            SizedBox(
-              height: 72,
-              child: Center(
-                child: ListTile(title: Text(t.settingsFontColorTitle)),
-              ),
-            ),
-            const Placeholder(fallbackHeight: 72),
-            SizedBox(
-              height: 72,
-              child: Center(
-                child: ListTile(title: Text(t.settingsFontSizeTitle)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
-              child: SegmentedButton<FontSize>(
-                segments: <ButtonSegment<FontSize>>[
-                  for (var fontSize in FontSize.values)
-                    ButtonSegment(
-                      value: fontSize,
-                      label: Text(t.settingsFontSizeValue(fontSize.name)),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 20),
+                      child: SegmentedButton<ThemeMode>(
+                        segments: <ButtonSegment<ThemeMode>>[
+                          for (var themeMode in ThemeMode.values)
+                            if (themeMode != ThemeMode.system)
+                              ButtonSegment(
+                                value: themeMode,
+                                label: Text(
+                                    t.settingsThemeModeValue(themeMode.name)),
+                              ),
+                        ],
+                        showSelectedIcon: true,
+                        selected: {model.themeMode},
+                        onSelectionChanged: (selected) {
+                          model.themeMode = selected.first;
+                        },
+                      ),
                     ),
-                ],
-                showSelectedIcon: true,
-                selected: {model.fontSize},
-                onSelectionChanged: (selected) {
-                  model.fontSize = selected.first;
-                },
-              ),
-            ),
-            SizedBox(
-              height: 88,
-              child: Center(
-                child: ListTile(
-                  title: Text(t.settingsFlushAtTitle),
-                  trailing: Text(model.flushAt.format(context)),
-                  onTap: () async {
-                    final tod = await showTimePicker(
-                      context: context,
-                      initialEntryMode: TimePickerEntryMode.dialOnly,
-                      initialTime: model.flushAt,
-                      builder: (context, child) {
-                        return MediaQuery(
-                          data: MediaQuery.of(context)
-                              .copyWith(alwaysUse24HourFormat: true),
-                          child: child!,
-                        );
-                      },
-                    );
+                    SizedBox(
+                      height: 72,
+                      child: Center(
+                        child: ListTile(title: Text(t.settingsFontColorTitle)),
+                      ),
+                    ),
+                    Container(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 16, bottom: 16),
+                        child: Row(
+                          children: [
+                            for (Color color in colorPallet)
+                              GestureDetector(
+                                onTap: () {
+                                  model.fontColor = color;
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 12),
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 1,
+                                          color: theme.colorScheme.outline),
+                                      shape: BoxShape.circle,
+                                      color: color),
+                                  child: model.fontColor == color
+                                      ? Icon(Icons.check,
+                                          color: theme
+                                              .colorScheme.onInverseSurface)
+                                      : null,
+                                ),
+                              )
+                          ],
+                        )),
+                    SizedBox(
+                      height: 72,
+                      child: Center(
+                        child: ListTile(title: Text(t.settingsFontSizeTitle)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 20),
+                      child: SegmentedButton<FontSize>(
+                        segments: <ButtonSegment<FontSize>>[
+                          for (var fontSize in FontSize.values)
+                            ButtonSegment(
+                              value: fontSize,
+                              label:
+                                  Text(t.settingsFontSizeValue(fontSize.name)),
+                            ),
+                        ],
+                        showSelectedIcon: true,
+                        selected: {model.fontSize},
+                        onSelectionChanged: (selected) {
+                          model.fontSize = selected.first;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 88,
+                      child: Center(
+                        child: ListTile(
+                          title: Text(t.settingsFlushAtTitle),
+                          trailing: Text(model.flushAt.format(context)),
+                          onTap: () async {
+                            final tod = await showTimePicker(
+                              context: context,
+                              initialEntryMode: TimePickerEntryMode.dialOnly,
+                              initialTime: model.flushAt,
+                              builder: (context, child) {
+                                return MediaQuery(
+                                  data: MediaQuery.of(context)
+                                      .copyWith(alwaysUse24HourFormat: true),
+                                  child: child!,
+                                );
+                              },
+                            );
 
-                    if (tod != null) {
-                      model.flushAt = tod;
-                    }
-                  },
+                            if (tod != null) {
+                              model.flushAt = tod;
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 88,
+                      child: Center(
+                        child: ListTile(
+                          title: Text(t.settingsHapticTitle),
+                          subtitle: Text(t.settingsHapticSubtitle),
+                          trailing: Switch(
+                            value: true,
+                            onChanged: (value) {},
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 88,
-              child: Center(
-                child: ListTile(
-                  title: Text(t.settingsSoundTitle),
-                  subtitle: Text(t.settingsSoundSubtitle),
-                  trailing: Switch(
-                    value: true,
-                    onChanged: (value) {},
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 88,
-              child: Center(
-                child: ListTile(
-                  title: Text(t.settingsHapticTitle),
-                  subtitle: Text(t.settingsHapticSubtitle),
-                  trailing: Switch(
-                    value: true,
-                    onChanged: (value) {},
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
