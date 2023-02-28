f6749bafa33aadcb177ec1f9b02c5677b4dea0ad
flash <developsoon@gmail.com>
2023-02-28 17:48:41 +0900

    chore: add default theme color

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -5,8 +5,11 @@ const lightColorScheme = ColorScheme.highContrastLight(
     primary: Color(0xff4455BA),
     onPrimaryContainer: Color(0xff000f5d),
     outline: Color(0xff767680),
+    secondary: Color(0xff5b5d72),
     secondaryContainer: Color(0xffE0E1F9),
+    tertiary: Color(0xff77536d),
     error: Color(0xffBA1A1A),
     surfaceTint: Color(0xff4455ba),
     onSurface: Color(0xff1B1B1F),
+    onInverseSurface: Color(0xfff3f0f4),
     onSurfaceVariant: Color(0xff46464F));
