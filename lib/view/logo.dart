ce988eae64607881d672d20c04f11b8bebdb5592
flash <developsoon@gmail.com>
2023-03-22 10:45:12 +0900

    fix: apply theme mode color instead of default dynamic colors

--- lib/view/logo.dart
+++ lib/view/logo.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_todo/models/settings.dart';
+import 'package:simple_todo/theme/dark_theme.dart';
+import 'package:simple_todo/theme/light_theme.dart';
 
 class Logo extends StatelessWidget {
   const Logo({
     super.key,
   });
 
+  bool _isDark(ThemeMode themeMode) {
+    return themeMode == ThemeMode.dark;
+  }
+
   @override
   Widget build(BuildContext context) {
+    ThemeMode themeMode = context.watch<SettingsModel>().themeMode;
     return SvgPicture.asset(
       'assets/logo.svg',
       colorFilter: ColorFilter.mode(
-        Theme.of(context).colorScheme.onPrimaryContainer,
+        _isDark(themeMode)
+            ? darkColorScheme.onPrimaryContainer
+            : lightColorScheme.onPrimaryContainer,
         BlendMode.srcIn,
       ),
     );
